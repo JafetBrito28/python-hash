d619049e79e02d0808920512eb9ed1a75baa985e
Jafet Brito <67519851+JafetBrito28@users.noreply.github.com>
2023-06-18 01:11:14 -0600

    create string-py
    Commit  one, the first one...

---
+++ Level 1/string.py
@@ -0,0 +1,14 @@
+# Strings
+
+apollo = "Apollo"
+zeus = "Zeus"
+hermes = "Hermes"
+athena = "Athena"
+poseidon = "Poseidon"
+aphrodite = "Aphrodite"
+hera = "Hera"
+ares = "Ares"
+dionysus = "Dionysus"
+hades = "Hades"
+
+print(apollo) # Prints "Apollo"
