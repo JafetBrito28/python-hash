df81cd12a85d394e59038f6d4f0f44cc4a4c426b
Jafet Brito <67519851+JafetBrito28@users.noreply.github.com>
2023-06-18 01:27:12 -0600

    create arith.py
    numbers...

---
+++ Level 1/arith.py
@@ -0,0 +1,14 @@
+# Arithmetics
+
+print(10 + 10) # Prints 20
+print(10 - 10) # Prints 0
+print(10 * 10) # Prints 100 
+print(10 / 10) # Prints 1 
+print(10 % 10) # Prints 0
+print(10 ** 10) # Prints 1000
+print(10 // 3) # Prints 3
+
+print(2 ** 3 + 4 * 7 //5)
+
+print("Hack" + " " + "the" + " " + "world") # Prints "Hack the world"
+print( "hello" * 3 ) # Prints "hellohellohello"
